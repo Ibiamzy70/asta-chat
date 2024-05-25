b03e8708117d66cccec5a06554b499c2a6223b30
Ibiamzy70 <111053630+Ibiamzy70@users.noreply.github.com>
2024-05-24 23:43:12 -0400

    my react app

---
+++ my-react-app/index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './my-react-app/App'; // Import your main component
+
+// Register the main component of your app
+AppRegistry.registerComponent('Asta chat', () => App);
